05cd95e169d0a875e0df4407cd34c7a93e3e78e5
Shriee <shriee05@gmail.com>
2026-07-25 21:21:24 +0530

    final integration

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 int main(){
 int a=10;
 int b=5;
-printf("addition = %d\n,add(a,b));
-printf("subtraction = %d\n,sub(a,b));
-printf("multiplictaion = %d\n,mul(a,b));
+printf("addition = %d\n",add(a,b));
+printf("subtraction = %d\n",sub(a,b));
+printf("multiplictaion = %d\n",mul(a,b));
 return 0;
 }
