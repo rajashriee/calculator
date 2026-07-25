e1fcfd6e7db8f16afaeff31ed5dc653fe2bacdf3
Shriee <shriee05@gmail.com>
2026-07-25 21:00:41 +0530

    cli integrated

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int mian(){
-printf("hello from cli");
-retuen 0;
+#include "add.h"
+#include "sub.h"
+#include "mul.h"
+int main(){
+int a=10;
+int b=5;
+printf("addition = %d\n,add(a,b));
+printf("subtraction = %d\n,sub(a,b));
+printf("multiplictaion = %d\n,mul(a,b));
+return 0;
 }
